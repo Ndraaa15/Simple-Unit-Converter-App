9bcdd6810c80c00a5e8ede0bbefbd69bc8f4cc2c
cupcakez <cupcake@xoo.localdomain>
2024-11-27 11:58:55 +0700

    add another composable component

--- app/src/main/java/my/id/cupcakez/unitconverterapp/MainActivity.kt
+++ app/src/main/java/my/id/cupcakez/unitconverterapp/MainActivity.kt
@@ -1,21 +1,33 @@
 package my.id.cupcakez.unitconverterapp
 
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import my.id.cupcakez.unitconverterapp.ui.theme.UnitConverterAppTheme
@@ -42,23 +54,60 @@ fun UnitConverter(modifier: Modifier) {
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
             value = "",
             placeholder = { Text("Enter value") },
             onValueChange = {
-            // here hoes what shsould happen when the value changes }
+            // here hoes what should happen when the value changes }
             } ,
             modifier = Modifier.fillMaxWidth()
         )
         // UI elements are placed in a column
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
-
+            Box{
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Select")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Feets") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Inches") }, onClick = {})
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Box{
+                // Text is a composable that displays text
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Select")
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Feets") }, onClick = {})
+                    DropdownMenuItem(text = { Text("Inches") }, onClick = {})
+                }
+            }
+            
         }
-
-        Text("Result : ")
+//        // context is a variable that holds the current context
+//        val context = LocalContext.current
+//
+//        // Button is a material design button
+//        // The onClick parameter is a lambda that is called when the button is clicked
+//        // Toast is a message that pops up on the screen
+//        Button(onClick = { Toast.makeText(context, "Thanks for clicking", Toast.LENGTH_LONG).show() }) {
+//            Text("Click Me")
+//        }
+//        Text("Result : ")
     }
 }
 
